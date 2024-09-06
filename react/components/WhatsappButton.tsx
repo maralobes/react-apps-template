22c6d95a360d44c07446a3f0b8f1ce08e0bc8db5
Maria <maria.villaloboschaves@gmail.com>
2024-09-06 12:56:30 -0600

    feature: create whatsapp button component

--- react/components/WhatsappButton.tsx
+++ react/components/WhatsappButton.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
-const WhatsappButton = () => {
-
-    return (<></>)
+type Props = {
+    logo: string
+    phone: string
+    message: string
+    width: number
+    height: number
+}
+const WhatsappButton = ({logo, phone, message, width, height}: Props) => {
+    console.log(logo, phone, message, width, height)
+    return (
+        <div className='fixed'>
+            <a href="">
+                <img 
+                    src={logo}
+                    alt={message}
+                    width= {width}
+                    height= {height}
+                />
+            </a>
+        </div>
+    )
 }
 
-export default WhatsappButton
+export default WhatsappButton;
