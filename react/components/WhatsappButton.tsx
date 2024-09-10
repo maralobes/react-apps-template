154ea2cf986e92f63b0ea58d57e90d2f5b5e7762
Maria <maria.villaloboschaves@gmail.com>
2024-09-10 17:10:10 -0600

    feature: Whatsapp buttom customization

--- react/components/WhatsappButton.tsx
+++ react/components/WhatsappButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 type Props = {
     logo: string
@@ -8,10 +9,15 @@ type Props = {
     height: number
 }
 const WhatsappButton = ({logo, phone, message, width, height}: Props) => {
-    console.log(logo, phone, message, width, height)
+    const formatMessage = message.replace(/ /g, "%20")
     return (
         <div className='fixed'>
-            <a href="">
+            <a 
+                href={`https://wa.me/${phone}?text=${formatMessage}`}
+                target='_blank'
+                rel='noreferrer noopener'
+                >
+                
                 <img 
                     src={logo}
                     alt={message}
@@ -21,6 +27,58 @@ const WhatsappButton = ({logo, phone, message, width, height}: Props) => {
             </a>
         </div>
     )
+}
+WhatsappButton.propTypes  = {
+    logo: PropTypes.string.isRequired, 
+    phone: PropTypes.string.isRequired,
+    message: PropTypes.string, 
+    width: PropTypes.string, 
+    height: PropTypes.string
+}
+
+WhatsappButton.defaultProps = {
+    "logo": "logo.png",
+    "phone": "89760045",
+    "message": "Hola, necesita ayuda?",
+    "width": 50,
+    "height": 50
+}
+
+WhatsappButton.schema = {
+    title: "Whatsapp button",
+    type: "object",
+    properties: {
+        logo: {
+            title: "Whatsapp logo",
+            type: "string",
+            widget: {
+                "ui:widget": "image-uploader"
+            }
+        },
+        phone: {
+            title: "Whatsapp phone",
+            type: "string",
+            description: "Please, add a phone number"
+        },
+        message: {
+            title: "Whatsapp welcome message",
+            type: "string",
+            description: "Please, add a message for your customer",
+            widget: {
+                "ui:widget": "textarea"
+            }
+        },
+        width: {
+            title: "Whatsapp logo width",
+            type: "string",
+            description: "Please, add the desire width of the Whatsapp logo"
+        },
+        height: {
+            title: "Whatsapp logo height",
+            type: "string",
+            description: "Please, add the desire height of the Whatsapp logo"
+        }
+    }
 }
 
 export default WhatsappButton;
